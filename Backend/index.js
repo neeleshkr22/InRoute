3581b16758c696d7a2d292fedf6404ace443da93
Neelesh <130912265+neeleshkr22@users.noreply.github.com>
2025-01-24 23:22:57 +0530

    commit

---
+++ Backend/index.js
@@ -0,0 +1,28 @@
+import db from "./Database/db.js";
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+
+const allowedOrigins = ["http://localhost:3000"];
+
+app.use(cors({
+    origin: allowedOrigins,
+    credentials: true,
+}));
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get("/",(req,res)=>{
+    res.send("Hello World");
+})
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
+    db();
+})
